ca9c35ae466a84ed5cc0c5f85cbd3cc03880caf9
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-08-19 18:36:32 +0200

    feat(activity): add support for automatic Pipedrive activity creation on hook trigger

---
+++ src/_COMPONENTS/HOOK/HooksParameters/CreateActivity.tsx
@@ -0,0 +1,220 @@
+import Input from '_COMPONENTS/FORMS/INPUT/input/Input'
+import Select from '_COMPONENTS/FORMS/INPUT/select/Select'
+import { appDataStore } from '_STORES/AppData'
+import { hookStore } from '_STORES/Hooks'
+import { classNames, updateIn, useCallPipedriveApi } from 'utils/helpers'
+import { observer } from 'mobx-react-lite'
+import React from 'react'
+import { translate } from 'translation'
+import { Hook } from 'Types'
+import { useLoadPipedriveUsers } from 'utils/pipedrive'
+import InputCheckbox from '_COMPONENTS/FORMS/InputCheckbox/InputCheckbox'
+
+function CreateActivity({ hook }: { hook: Hook }) {
+
+
+    const callPipedriveApi = useCallPipedriveApi()
+
+    const activityTypesOptions =
+        [{
+            id: 0,
+            value: 0,
+            label: translate('No activity type')
+        }].concat(
+            appDataStore.appData.parameters.pipedrive.activityTypes.map((type, index) => ({
+                id: index,
+                value: type.id,
+                label: type.name
+            }))
+        )
+
+    const pipedriveUsersOptions =
+        [{
+            id: 0,
+            value: 0,
+            label: translate('No owner')
+        }].concat(
+            appDataStore.appData.parameters.pipedrive.users.map((user, index) => ({
+                id: user.id,
+                value: user.id,
+                label: user.name
+            }))
+        )
+
+    const loadPipedriveUsers = useLoadPipedriveUsers()
+
+    const loadActivityTypes = (e: React.FormEvent) => {
+        callPipedriveApi("activityTypes", null, null, null, e)
+            .then(res => {
+                if (res) {
+                    const newAppDataStore = appDataStore.appData
+                    newAppDataStore.parameters.pipedrive.activityTypes = res.data.data
+                    appDataStore.setAppData(newAppDataStore)
+                }
+            })
+    }
+
+    return (
+        <div className='flex gap-4'>
+            <div className='flex-1 pr-2 border-r'>
+                <div className='mb-2 w-full'>
+                    <div className='font-semibold'>Subject of the activity</div>
+                    <Input
+                        className='!w-full'
+                        type='text'
+                        value={hook.option.activity?.subject ?? `${hook.label} triggered`}
+                        onInput={value =>
+                            hookStore.updateHook(
+                                hook.id,
+                                updateIn(hook, ["option", "activity", "subject"], value)
+                            )
+                        }
+                    />
+                </div>
+
+                <div>
+                    <div className='font-semibold'>Notes</div>
+                    <textarea
+                        className='p-2 border border-[rgb(0, rounded-md w-full min-h-[100px] 0, 0, 0.12)]'
+                        onChange={e => hookStore.updateHook(
+                            hook.id,
+                            updateIn(hook, ["option", "activity", "note"], e.target.value)
+                        )}
+                    >
+                        {hook.option.activity?.note ?? ''}
+                    </textarea>
+
+                    <p className='mt-2'>Activities are automatically linked to the related person, organization, and order that triggered the event</p>
+                </div>
+            </div>
+
+            <div className='flex-1'>
+                <div className='mb-2'>
+                    <div className='font-semibold'>Activity type (optional)</div>
+                    <div className='flex items-end gap-4 max-w-[500px]'>
+                        <Select
+                            className='flex-1'
+                            options={activityTypesOptions}
+                            onSelect={value =>
+                                hookStore.updateHook(
+                                    hook.id,
+                                    updateIn(hook, ["option", "activity", "type"], value)
+                                )
+                            }
+                            value={hook.option.activity?.type ?? activityTypesOptions[0].value}
+                        />
+                        <form onSubmit={e => loadActivityTypes(e)}>
+                            <button
+                                className='light-button'
+                            >
+                                {appDataStore.appData.parameters.pipedrive.activityTypes.length
+                                    ? translate("Reload pipedrive's activity types")
+                                    : translate("Load pipedrive's activity Type")
+                                }
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div className='mb-2'>
+                    <div className='font-semibold'>Owner (optional)</div>
+                    <div className='flex items-end gap-4 max-w-[500px]'>
+                        <Select
+                            className='flex-1'
+                            options={pipedriveUsersOptions}
+                            onSelect={value =>
+                                hookStore.updateHook(
+                                    hook.id,
+                                    updateIn(hook, ["option", "activity", "owner_id"], value)
+                                )
+                            }
+                            value={hook.option.activity?.owner_id ?? pipedriveUsersOptions[0].value}
+                        />
+                        <form onSubmit={e => loadPipedriveUsers(e)}>
+                            <button
+                                className='light-button'
+                            >
+                                {appDataStore.appData.parameters.pipedrive.users.length
+                                    ? translate("Reload pipedrive's users")
+                                    : translate("Load pipedrive's users")
+                                }
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div className='mt-4'>
+                    <InputCheckbox
+                        checked={hook.option.activity?.done ?? false}
+                        onChange={value =>
+                            hookStore.updateHook(
+                                hook.id,
+                                updateIn(hook, ["option", "activity", "done"], value)
+                            )
+                        }
+                        label='Mark activity as done'
+                    />
+
+                    {/* <RenderIf condition={!hook.option.activity?.done}> */}
+                    <div className='mt-2'>
+                        <div className='font-semibold'>Due date</div>
+                        <div className='flex items-center gap-2'>
+                            <InputCheckbox
+                                checked={hook.option.activity?.enable_due_date ?? false}
+                                onChange={value => hookStore.updateHook(
+                                    hook.id,
+                                    updateIn(hook, ["option", "activity", "enable_due_date"], value)
+                                )}
+                            />
+                            <div className={classNames(
+                                "condition-maker transition-all duration-300 ease-in-out"
+                                , hook.option.activity?.enable_due_date ? "opacity-100" : "opacity-50")
+                            }>
+                                <div>After </div>
+                                <Input
+                                    type='number'
+                                    min='0'
+                                    max='30'
+                                    step='1'
+                                    value={`${hook.option.activity?.due_date_value ?? 1}`}
+                                    onInput={value =>
+                                        hookStore.updateHook(
+                                            hook.id,
+                                            updateIn(hook, ["option", "activity", "due_date_value"], value)
+                                        )
+                                    }
+                                />
+                                <select
+                                    value={hook.option.activity?.due_date_unit ?? "day"}
+                                    onChange={e => hookStore.updateHook(
+                                        hook.id,
+                                        updateIn(hook, ["option", "activity", "due_date_value"], e.target.value)
+                                    )}
+                                >
+                                    <option value="day">{translate("DAY")}</option>
+                                    <option value="week">{translate("WEEK")}</option>
+                                    <option value="month">{translate("MONTH")}</option>
+                                </select>
+                                at
+                                <Input
+                                    type='time'
+                                    step="60"
+                                    value={hook.option.activity?.due_date_time ?? "09:00"}
+                                    onInput={value =>
+                                        hookStore.updateHook(
+                                            hook.id,
+                                            updateIn(hook, ["option", "activity", "due_date_time"], value)
+                                        )
+                                    }
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* </RenderIf> */}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default observer(CreateActivity)
--- src/_COMPONENTS/HOOK/HooksParameters/HookParameters.tsx
+++ src/_COMPONENTS/HOOK/HooksParameters/HookParameters.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { translate } from 'translation'
 import { Hook } from 'Types'
 import CustomHooksParameters from './CustomHooksParameters'
+import { hookStore } from '_STORES/Hooks'
+import RenderIf from '_COMPONENTS/GENERAL/RenderIf'
+import { updateIn } from 'utils/helpers'
+import CreateActivity from './CreateActivity'
+import Tooltip from '_COMPONENTS/GENERAL/ToolType/ToolType.'
 
 export default function HookParameters({ hook }: { hook: Hook }) {
     return (
@@ -9,22 +14,32 @@ export default function HookParameters({ hook }: { hook: Hook }) {
             <h5 className='m-b-25'>{translate(`Parameter of the event : ${hook.label}`)}</h5>
 
             {/* C'est clairement la V2 : ajout d'une activité avec des infos */}
-            {/* <label className='pointer flex items-center m-b-10'>
-            <input
-            type='checkbox'
-            className='m-r-10'
-            onChange={e => hook && updateHook(hook.id, 'option.createActivity', e.target.checked)}
-            checked={hook.option.createActivity ?? false}
-            />
-            <div>
-            {translate('Add an activity when this event is triggered.')}
-            <div className='subtext'>
-            {translate(
-                `An activity specifying the updated fields and the triggered event will be automatically added to the ${hook.category} on Pipedrive.`
-                )}
+            <label className='flex items-center m-b-10 pointer'>
+                <input
+                    type='checkbox'
+                    className='m-r-10'
+                    onChange={e => hook &&
+                        hookStore.updateHook(
+                            hook.id,
+                            updateIn(hook, ["option", "createActivity"], e.target.checked)
+                        )}
+                    checked={hook.option.createActivity ?? false}
+                />
+                <div>
+                    <Tooltip
+                        tooltipText={<div>
+                            <p className='mb-1'>Example: When a new account is created (User Registration), you can automatically schedule an “Onboarding call” in Pipedrive. </p>
+                            <p>By setting the due date to 2 days later at 09:00, your sales team receives a timely reminder to follow up, ensuring every new customer gets a smooth start.</p>
+                        </div>}
+                        mainText={translate('Add an activity when this event is triggered')} />
                 </div>
+            </label>
+
+            <RenderIf condition={!!hook.option.createActivity}>
+                <div className='bg-pipedriveMain-200/10 mb-4 p-2 border rounded-lg'>
+                    <CreateActivity hook={hook} />
                 </div>
-                </label> */}
+            </RenderIf>
 
             <CustomHooksParameters hook={hook} />
 
